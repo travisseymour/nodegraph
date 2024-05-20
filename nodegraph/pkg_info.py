66e7bf728df9e053a8df7c7ba297b0da5ad679dd
Travis L. Seymour <nogard@ucsc.edu>
2024-05-19 17:29:38 -0700

    attempting to update version to see if pip picks it up

--- nodegraph/pkg_info.py
+++ nodegraph/pkg_info.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-__version__ = '0.2.3'
+__version__ = '0.2.3.2'
 __status__ = 'Work in Progress'
 __license__ = 'MIT'
 
-__authors__ = 'Mark Dasco, Johnny Chan'
+__authors__ = 'Mark Dasco, Johnny Chan, Travis Seymour'
 __porter__ = 'fivedbrothers'
 
 __module_name__ = 'nodegraph'
